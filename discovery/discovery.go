da1b9f3d9f6e4e0df35eaa11014c44cf0f0e01f1
Angie Ramirez <angie@livepeer.org>
2019-02-01 18:40:42 -0500

    moving randomization to GetOrchestrators

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -17,9 +17,18 @@ import (
 
 const GetOrchestratorsTimeoutLoop = 1 * time.Hour
 
+type rpcMethods interface {
+	getOrchestrator(context.Context, *url.URL) (*net.OrchestratorInfo, error)
+}
+
 type orchestratorPool struct {
 	uris  []*url.URL
 	bcast server.Broadcaster
+	rpc   rpcMethods
+}
+
+func (o *orchestratorPool) getOrchestrator(ctx context.Context, orchestratorServer *url.URL) (*net.OrchestratorInfo, error) {
+	return server.GetOrchestratorInfo(ctx, o.bcast, orchestratorServer)
 }
 
 func NewOrchestratorPool(node *core.LivepeerNode, addresses []string) *orchestratorPool {
@@ -41,8 +50,14 @@ func NewOrchestratorPool(node *core.LivepeerNode, addresses []string) *orchestra
 		glog.Error("Could not parse orchAddresses given - no URIs returned ")
 	}
 
+	var randomizedUris []*url.URL
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, i := range r.Perm(len(uris)) {
+		uri := uris[i]
+		randomizedUris = append(randomizedUris, uri)
+	}
 	bcast := core.NewBroadcaster(node)
-	return &orchestratorPool{bcast: bcast, uris: uris}
+	return &orchestratorPool{bcast: bcast, uris: randomizedUris}
 }
 
 func NewOnchainOrchestratorPool(node *core.LivepeerNode) *orchestratorPool {
@@ -73,9 +88,10 @@ func (o *orchestratorPool) GetOrchestrators(numOrchestrators int) ([]*net.Orches
 	numResp := 0
 	numSuccessResp := 0
 	respLock := sync.Mutex{}
+	orchChanLock := sync.Mutex{}
 
 	getOrchInfo := func(uri *url.URL) {
-		info, err := server.GetOrchestratorInfo(ctx, o.bcast, uri)
+		info, err := o.rpc.getOrchestrator(ctx, uri)
 		respLock.Lock()
 		defer respLock.Unlock()
 		numResp++
@@ -83,10 +99,7 @@ func (o *orchestratorPool) GetOrchestrators(numOrchestrators int) ([]*net.Orches
 			orchInfos = append(orchInfos, info)
 			numSuccessResp++
 		}
-		if numResp >= len(o.uris) {
-			rand.Seed(time.Now().Unix())
-			n := rand.Int() % len(o.uris)
-			orchInfos = []*net.OrchestratorInfo{orchInfos[n]}
+		if numSuccessResp >= numOrchestrators || numResp >= len(o.uris) {
 			orchChan <- struct{}{}
 		}
 	}
@@ -97,12 +110,15 @@ func (o *orchestratorPool) GetOrchestrators(numOrchestrators int) ([]*net.Orches
 
 	select {
 	case <-ctx.Done():
-		glog.Info("Done fetching orch info for orchestrators, context timeout: ", orchInfos)
+		glog.Error("Done fetching orch info for orchestrators, context timeout: ", orchInfos)
 		cancel()
-		return orchInfos, nil
+		return orchInfos[:numOrchestrators], nil
 	case <-orchChan:
-		glog.Info("Done fetching orch info for orchestrators, numResponses fetched: ", orchInfos)
+		orchChanLock.Lock()
+		returnOrchs := orchInfos[:numOrchestrators]
+		orchChanLock.Unlock()
+		glog.Error("Done fetching orch info for orchestrators, numResponses fetched: ", orchInfos)
 		cancel()
-		return orchInfos, nil
+		return returnOrchs, nil
 	}
 }
--- discovery/discovery_test.go
+++ discovery/discovery_test.go
@@ -1,6 +1,7 @@
 package discovery
 
 import (
+	"context"
 	"net/url"
 	"testing"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/livepeer/go-livepeer/core"
 	"github.com/livepeer/go-livepeer/eth"
 	lpTypes "github.com/livepeer/go-livepeer/eth/types"
+	"github.com/livepeer/go-livepeer/net"
 	"github.com/livepeer/go-livepeer/server"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -96,18 +98,41 @@ func TestNewDBOrchestratorPoolCache_GivenListOfOrchs_CreatesPoolCacheCorrectly(t
 	require.NotNil(dbOrch)
 }
 
+type stubRpc struct {
+}
+
+func (s *stubRpc) getOrchestrator(ctx context.Context, orchestratorServer *url.URL) (*net.OrchestratorInfo, error) {
+	return &net.OrchestratorInfo{Transcoder: orchestratorServer.String()}, nil
+}
+
+func TestTempGetOrchestrators_GivenOneOrchestratorRequested_ReturnsRandomOrch(t *testing.T) {
+	dbh, dbraw, err := common.TempDB(t)
+	defer dbh.Close()
+	defer dbraw.Close()
+	require := require.New(t)
+	assert := assert.New(t)
+	require.Nil(err)
+
+	addresses := []string{"https://127.0.0.1:8936", "https://127.0.0.1:8937", "https://127.0.0.1:8938"}
+
+	node, _ := core.NewLivepeerNode(nil, "", nil)
+	orchPool := NewOrchestratorPool(node, addresses)
+	orchPool.rpc = &stubRpc{}
+
+	orchInfo, err := orchPool.GetOrchestrators(1)
+	require.Nil(err)
+	assert.Len(orchInfo, 1)
+}
+
 func TestNewOrchestratorPoolCache_GivenListOfOrchs_CreatesPoolCacheCorrectly(t *testing.T) {
 	node, _ := core.NewLivepeerNode(nil, "", nil)
 	addresses := []string{"https://127.0.0.1:8936", "https://127.0.0.1:8937", "https://127.0.0.1:8938"}
 	expectedOffchainOrch := StubOrchestratorPool(addresses)
 	assert := assert.New(t)
-	require := require.New(t)
 
 	// creating NewOrchestratorPool with orch addresses
 	offchainOrch := NewOrchestratorPool(node, addresses)
-	for i, uri := range offchainOrch.uris {
-		assert.Equal(uri.String(), expectedOffchainOrch.uris[i].String())
-	}
+	assert.Len(offchainOrch.uris, 3)
 
 	// creating new OrchestratorPool with different first value
 	addresses[0] = "https://127.0.0.1:89"
@@ -117,12 +142,14 @@ func TestNewOrchestratorPoolCache_GivenListOfOrchs_CreatesPoolCacheCorrectly(t *
 	orchestrators := StubOrchestrators(addresses)
 	node.Eth = &eth.StubClient{Orchestrators: orchestrators}
 
-	expectedRegisteredTranscoders, err := node.Eth.RegisteredTranscoders()
-	require.Nil(err)
-
 	// testing NewOnchainOrchestratorPool
 	offchainOrchFromOnchainList := NewOnchainOrchestratorPool(node)
-	for i, uri := range offchainOrchFromOnchainList.uris {
-		assert.Equal(uri.String(), expectedRegisteredTranscoders[i].ServiceURI)
+	newAddressExists := false
+	for _, uri := range offchainOrchFromOnchainList.uris {
+		if uri.String() == addresses[0] {
+			newAddressExists = true
+		}
 	}
+
+	assert.True(newAddressExists)
 }
